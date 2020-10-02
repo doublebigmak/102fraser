e4bdf2419f91009cf251241b36a40bab3cf2aa91
doublebigmak <mak.michael.609@gmail.com>
2020-10-01 20:33:45 -0700

    js try 2

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,9 @@
 jQuery('a').click(function(e) {
-    e.preventDefault()
-    $(this).tab('show')
-  })
+    e.preventDefault();
+    $(this).tab('show');
+  });
+
+$('.nav-tabs a').click(function (e) {
+    e.preventDefault();
+    $(this).parent().removeClass('active');
+});
